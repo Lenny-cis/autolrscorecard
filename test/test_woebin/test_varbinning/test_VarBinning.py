73446fa44062950d0d1b0da4c1878b7f5bcb4088
Lenny-cis <ljn9008@163.com>
2021-08-08 18:03:51 +0800

    修改varbining的测试用例

--- test/test_woebin/test_varbinning/test_VarBinning.py
+++ test/test_woebin/test_varbinning/test_VarBinning.py
@@ -6,15 +6,17 @@ Created on Tue Aug  3 16:58:27 2021
 """
 
 import os
+import sys
 import numpy as np
 import pandas as pd
 import time
 from joblib import load, dump
 root = os.path.abspath(os.path.join(os.path.dirname(__file__), r'..\..\..\..'))
+sys.path.append(root)
 os.chdir(root)
 from autolrscorecard.woebin import VarBinning
 from autolrscorecard import alsc_parallel
-alsc_parallel.enable()
+alsc_parallel.close()
 data_file = os.path.join(root, 'autolrscorecard', 'test', 'data',
                           'test_sample.h5')
 data = pd.read_hdf(data_file, 'data')
